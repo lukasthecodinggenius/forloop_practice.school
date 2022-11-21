fecb9955378a1428c5abb66481d04fee11e3faef
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2022-11-21 13:15:40 -0500

    Create example.cs

---
+++ example.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+ public static void Main(string[] args)	
+ {
+for (int i = 0; i < 5; i++)
+{
+	Console.WriteLine(i);
+}
+/* initializzation sets a variable before the loop starts (int i = 0).
+
+Condition defines the condition for the loop to run (it must be less than 5). If the condition is true, the loop will sdtart over again, if it is false, the loop will end.
+
+update increases a value (i++) each time the code block in the loop has been executed. */
+ }
+}						
