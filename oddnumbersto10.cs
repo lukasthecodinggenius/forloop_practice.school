7981cbaa9491af33d71a5ba00e252285f228d7ae
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2022-11-21 13:34:43 -0500

    Create oddnumbersto10.cs

---
+++ oddnumbersto10.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+ public static void Main(string[] args)	
+ {
+for (int i = 1; i < 10; i += 2)
+{
+	Console.WriteLine(i);
+}
+	 //odd numbers between 1 and 10
+	 
+ }
+}						
+
+
