be6e2ac9a6e472e328a6d38a63a7087c47c82496
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2022-11-21 13:18:45 -0500

    Create evennumbersto10.cs

---
+++ evennumbersto10.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+ public static void Main(string[] args)	
+ {
+for (int i = 0; i <= 10; i += 2)
+{
+	Console.WriteLine(i);
+}
+//this code prints even numbers between 1 and 10
+ }
+}						
