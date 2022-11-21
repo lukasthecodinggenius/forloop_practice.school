a2b05443567a2c06ac0866ccaa81ceff0e60e7b1
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2022-11-21 13:24:52 -0500

    Create evennumbersto1.cs

---
+++ evennumbersto1.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+ public static void Main(string[] args)	
+ {
+for (int i = 10; i > 0; i--)
+{
+	Console.WriteLine(i);
+}
+//this code prints the numbers from 10 to 1
+ }
+}						
+
+
