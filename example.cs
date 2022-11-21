64f089f8bc6cbdd5e28ec7a0b12973d2782b4e9b
lukasthecodinggenius <92871428+lukasthecodinggenius@users.noreply.github.com>
2022-11-21 13:20:11 -0500

    Update example.cs

--- example.cs
+++ example.cs
@@ -8,10 +8,16 @@ for (int i = 0; i < 5; i++)
 {
 	Console.WriteLine(i);
 }
+	 
+//initialization is executed 1 time before the execution of the code block
+//condition defines the condition for executing the code block
+//update is executed every time after the code block has been executed
 /* initializzation sets a variable before the loop starts (int i = 0).
 
 Condition defines the condition for the loop to run (it must be less than 5). If the condition is true, the loop will sdtart over again, if it is false, the loop will end.
 
 update increases a value (i++) each time the code block in the loop has been executed. */
+
+
  }
 }						
